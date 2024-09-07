b7cf112f09a03627a6159c45c7668400e7724a9c
Sergey <barabashka089@gmail.com>
2024-09-07 11:51:12 +0300

    homework3

--- src/seminar/sem3/Main.java
+++ src/seminar/sem3/Main.java
@@ -1,5 +1,15 @@
 package seminar.sem3;
 
+//Создайте несколько экземпляров Pharmacy, причем, чтобы среди них было хотя бы два экземпляра
+//с полностью идентичными значениями полей.
+//
+//Сделать так, чтобы после выполнения кода ниже, в сете result были только уникальные объекты.
+//Set<Pharmacy> result = new HashSet<>(множество из нескольких лекарств);
+//System.out.println(result.size()); // тут вывод должен быть равен количеству УНИКАЛЬНЫХ экземпляров Pharmacy!
+//
+//Добавить интерфейс Copmparable<Pharmacy> к классу Pharmacy. Переопределить метод compareTo()
+//подсказка: можно добавить, а можно не добавлять в класс поля;
+
 import java.util.*;
 
 public class Main {
@@ -11,27 +21,58 @@ public class Main {
         PharmacyComponent pinicilin = new Pinicilin("Pinicilin", "10 Mg", 20);
         PharmacyComponent pinicilin2 = new Pinicilin("Pinicilin", "10 Mg", 20);
 
-        List<PharmacyComponent> pharmacyComponents = new ArrayList<>();
-        pharmacyComponents.add(azitronit);
-        pharmacyComponents.add(water);
-        pharmacyComponents.add(pinicilin);
-        pharmacyComponents.add(pinicilin2);
+//        List<PharmacyComponent> pharmacyComponents = new ArrayList<>();
+//        pharmacyComponents.add(azitronit);
+//        pharmacyComponents.add(water);
+//        pharmacyComponents.add(pinicilin);
+//        pharmacyComponents.add(pinicilin2);
+//
+//        System.out.println(pharmacyComponents);
+//        Collections.sort(pharmacyComponents);
+//        System.out.println(pharmacyComponents);
 
-        System.out.println(pharmacyComponents);
-        Collections.sort(pharmacyComponents);
-        System.out.println(pharmacyComponents);
+
+        Pharmacy pharmacy1 = new Pharmacy(1).addComponent(azitronit).addComponent(water);
+        Pharmacy pharmacy2 = new Pharmacy(2).addComponent(water).addComponent(pinicilin);
+        Pharmacy pharmacy3 = new Pharmacy(3).addComponent(water).addComponent(pinicilin);
+        Pharmacy pharmacy4 = new Pharmacy(4).addComponent(azitronit1).addComponent(pinicilin2);
+        Pharmacy pharmacy5 = new Pharmacy(5).addComponent(azitronit1).addComponent(pinicilin2);
+        Pharmacy pharmacy6 = new Pharmacy(6).addComponent(azitronit1).addComponent(pinicilin2);
+
+        Set<Pharmacy> result = new HashSet<>();
+        result.add(pharmacy1);
+        result.add(pharmacy2);
+        result.add(pharmacy3);
+        result.add(pharmacy4);
+        result.add(pharmacy5);
+        result.add(pharmacy6);
 
 
-        Pharmacy pharmacy1 = new Pharmacy().addComponent(azitronit).addComponent(water);
-        Pharmacy pharmacy2 = new Pharmacy().addComponent(water).addComponent(pinicilin);
-
-        for (PharmacyComponent component: pharmacy1){
-            System.out.println(component.getName());
-        }
+        System.out.println(result.size());
+        System.out.println(result);
+        System.out.println("--------------------------------------");
 
         List<Pharmacy> pharmacyList = new ArrayList<>();
+        pharmacyList.add(pharmacy3);
         pharmacyList.add(pharmacy1);
         pharmacyList.add(pharmacy2);
+        pharmacyList.add(pharmacy6);
+        pharmacyList.add(pharmacy4);
+        pharmacyList.add(pharmacy5);
+
+
+        System.out.println(pharmacyList);
+        Collections.sort(pharmacyList);
+        System.out.println(pharmacyList);
+
+
+//        for (PharmacyComponent component: pharmacy1){
+//            System.out.println(component.getName());
+//        }
+//
+//        List<Pharmacy> pharmacyList = new ArrayList<>();
+//        pharmacyList.add(pharmacy1);
+//        pharmacyList.add(pharmacy2);
 
 //        Collections.sort(pharmacyList);
 
@@ -45,11 +86,7 @@ public class Main {
 //            System.out.println(iterator.next());
 //        }
 
-        Set<PharmacyComponent> set = new HashSet<>();
-        set.add(azitronit);
-        set.add(azitronit1);
-        set.add(azitronit2);
-        System.out.println(set);
+
 
     }
 }
--- src/seminar/sem3/Pharmacy.java
+++ src/seminar/sem3/Pharmacy.java
@@ -3,12 +3,17 @@ package seminar.sem3;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
+
 // Лекарство
 public class Pharmacy implements Iterable<PharmacyComponent>, Comparable<Pharmacy>{
     private List<PharmacyComponent> components = new ArrayList<>();
     private int index = 0;
+    private int id = 0;
 
-
+    public Pharmacy(int id) {
+        this.id = id;
+    }
 
     public List<PharmacyComponent> getComponents() {
         return components;
@@ -44,8 +49,7 @@ public class Pharmacy implements Iterable<PharmacyComponent>, Comparable<Pharmac
     @Override
     public String toString() {
         return "Pharmacy{" +
-                "components=" + components +
-                ", index=" + index +
+                "components=" + components + ", id = " + id +
                 '}';
     }
 
@@ -56,13 +60,25 @@ public class Pharmacy implements Iterable<PharmacyComponent>, Comparable<Pharmac
 
     @Override
     public int compareTo(Pharmacy o) {
-        return 0;
+        if (this.id > o.id) return 1;
+        else if (this.id < o.id) return -1;
+        else return 0;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pharmacy pharmacy = (Pharmacy) o;
+        return index == pharmacy.index && Objects.equals(components, pharmacy.components);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(components, index);
+    }
 
-
-//    @Override
+    //    @Override
 //    public Iterator<PharmacyComponent> iterator() {
 //        return new Iterator<PharmacyComponent>() {
 //            @Override
